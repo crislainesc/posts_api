29aae7d26226d75a5c23d26b6a3c0faa9fc40c2c
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-06-07 19:04:24 -0300

    feat: users seed for fill database added

---
+++ database/seeders/User.ts
@@ -0,0 +1,15 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    const uniqueKey = 'email'
+
+    await User.updateOrCreateMany(uniqueKey, [
+      {
+        email: 'admin@admin.com',
+        password: 'admin',
+      },
+    ])
+  }
+}
